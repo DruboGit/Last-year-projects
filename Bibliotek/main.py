a1ff3dc8936da38a1422a0c89829be06d072278c
Diezel Löfgren <dilo1206@storsjogymnasiet.se>
2024-09-04 08:57:59 +0200

    Filled out medias class file and started work on main file

---
+++ Bibliotek/main.py
@@ -0,0 +1,8 @@
+import medias as m
+
+def main():
+    print("What would you like to do?\nAdd a book       (1)   (Admin access required)\nLook in library  (2)")
+    input()
+    input("No")
+
+main()
--- Bibliotek/medias.py
+++ Bibliotek/medias.py
@@ -20,3 +20,33 @@ class AudioBook(Media):
     def __init__(self, title: str, year: str, creator: str, borrowed: bool, place: str, reader : str):
         super().__init__(title, year, creator, borrowed, place)
         self.reader = reader
+
+class Fiction(Book):
+    def __init__(self, title: str, year: str, creator: str, borrowed: bool, place: str, publisher: str, genre: str):
+        super().__init__(title, year, creator, borrowed, place, publisher)
+        self.genre = genre
+
+class NonFiction(Book):
+    def __init__(self, title: str, year: str, creator: str, borrowed: bool, place: str, publisher: str, subject: str):
+        super().__init__(title, year, creator, borrowed, place, publisher)
+        self.subject = subject
+
+class FictionMovie(Movie):
+    def __init__(self, title: str, year: str, creator: str, borrowed: bool, place: str, director: str, genre: str):
+        super().__init__(title, year, creator, borrowed, place, director)
+        self.genre = genre
+
+class NonFictionMovie(Movie):
+    def __init__(self, title: str, year: str, creator: str, borrowed: bool, place: str, director: str, subject: str):
+        super().__init__(title, year, creator, borrowed, place, director)
+        self.subject = subject
+
+class FictionAudioBook(AudioBook):
+    def __init__(self, title: str, year: str, creator: str, borrowed: bool, place: str, reader: str, genre: str):
+        super().__init__(title, year, creator, borrowed, place, reader)
+        self.genre = genre
+    
+class NonFictionAudioBook(AudioBook):
+    def __init__(self, title: str, year: str, creator: str, borrowed: bool, place: str, reader: str, subject: str):
+        super().__init__(title, year, creator, borrowed, place, reader)
+        self.subject = subject
