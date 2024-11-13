6a92ae4299d6c8c7a3a7bea22a0188ed3d147331
Diezel Löfgren <dilo1206@storsjogymnasiet.se>
2024-11-13 09:39:50 +0100

    did stuff

--- Platformer/main.py
+++ Platformer/main.py
@@ -6,11 +6,13 @@ import sys
 pygame.init()
 vec = pygame.math.Vector2
 
-HEIGHT = 450
-WIDTH = 600
+HEIGHT = 650
+WIDTH = 800
 ACC = 0.5
 FRIC = -0.12
 FPS = 60
+
+x_offset = 50
 
 FramePerSec = pygame.time.Clock()
 displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -19,9 +21,13 @@ pygame.display.set_caption("Game")
 
 all_sprites = pygame.sprite.Group()
 all_sprites.add(objects.PT1)
+all_sprites.add(objects.PT2)
+all_sprites.add(objects.PT3)
 all_sprites.add(objects.P1)
 
 while True:
+
+    x_offset = objects.P1.pos.x - 400
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -36,7 +42,7 @@ while True:
     objects.P1.update()
     
     for entity in all_sprites:
-        displaysurface.blit(entity.surf, entity.rect)
+        displaysurface.blit(entity.surf, pygame.Rect(entity.rect.left - x_offset, entity.rect.top, entity.rect.width, entity.rect.height))
  
     pygame.display.update()
     FramePerSec.tick(FPS)
--- Platformer/objects.py
+++ Platformer/objects.py
@@ -1,8 +1,8 @@
 import pygame
 from pygame.locals import *
 
-HEIGHT = 450
-WIDTH = 600
+HEIGHT = 650
+WIDTH = 800
 ACC = 0.5
 FRIC = -0.12
 vec = pygame.math.Vector2
@@ -14,7 +14,7 @@ class Player(pygame.sprite.Sprite):
         self.surf.fill((128,255,40))
         self.rect = self.surf.get_rect(center = (10, 420))
 
-        self.pos = vec((10, 385))
+        self.pos = vec((50, 385))
         self.vel = vec(0,0)
         self.acc = vec(0,0)
 
@@ -31,11 +31,6 @@ class Player(pygame.sprite.Sprite):
         self.acc.x += self.vel.x * FRIC
         self.vel += self.acc
         self.pos += self.vel + 0.5 * self.acc
-
-        if self.pos.x > WIDTH:
-            self.pos.x = 0
-        if self.pos.x < 0:
-            self.pos.x = WIDTH
             
         self.rect.midbottom = self.pos
 
@@ -52,13 +47,17 @@ class Player(pygame.sprite.Sprite):
         
  
 class platform(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, pl_width, pl_height, pl_y, pl_x):
         super().__init__()
-        self.surf = pygame.Surface((400, 20))
+        self.surf = pygame.Surface((pl_width, pl_height))
         self.surf.fill((255,0,0))
-        self.rect = self.surf.get_rect(center = (WIDTH/2, HEIGHT - 10))
+        self.rect = self.surf.get_rect(center = (pl_x, pl_y))
  
-PT1 = platform()
+PT1 = platform(pl_width = 800, pl_height = 20, pl_x = WIDTH/2, pl_y = HEIGHT -10)
+PT2 = platform(pl_width=100, pl_height=20, pl_x=500, pl_y=HEIGHT - 30)
+PT3 = platform(pl_width=500, pl_height=20, pl_x=1000, pl_y=HEIGHT - 60)
+plats = [PT1,PT2,PT3]
 P1 = Player()
 platforms = pygame.sprite.Group()
-platforms.add(PT1)
+for i in plats:
+    platforms.add(i)
